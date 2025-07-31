86cd826045860d645a511c2fcc6c06d5f6f55a77
Pratyush Kashyap <pratyushk403@gmail.com>
2025-07-31 11:03:20 +0530

    Add Facebook Pixel ID 1638330460682991 for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,13 +53,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ''); // Pixel ID is empty, user to fill
+            fbq('init', '1638330460682991');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display:'none'}}
-            src="https://www.facebook.com/tr?id=&ev=PageView&noscript=1" // Pixel ID is empty
+            src="https://www.facebook.com/tr?id=1638330460682991&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
